6b9ee84983b201311ef797a0aaef5d4411f7716f
Matt <matthewhallowell1@gmail.com>
2021-01-01 13:07:34 -0600

    broke content parser into time and content methods

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ page = requests.get(URL)
 
 # Stores the names of all of the parks.
 parks = []
-trail_status = []
 
 def cleanhtml(raw_html):
     cleanr = re.compile('<.*?>')
@@ -21,7 +20,9 @@ def populate_park_list(threerivers_content):
     for tag in h3s:
         parks.append(cleanhtml(str(tag.contents[0])))
 
-def trail_status_extractor(trail_paragraph):
+# extracts the trail condition content from the trail paragraph
+# note: I decided to separate the content and time content from a shared method to allow for more specialized parsing for data storage in the future
+def extract_trail_conditions(trail_paragraph):
     content_list = list(trail_paragraph.children)
     if len(content_list) != 0:
         print('extracting page contents...')
@@ -29,16 +30,28 @@ def trail_status_extractor(trail_paragraph):
         # In the future, it would be better to dynamically find this in the event that the website maintainers end up manually changing the contents, therefore ruining how this works
         for i in range(19, len(content_list)):
             if content_list[i].name == "p":
-                # print(f"{i}: {content_list[i]}")
                 tag_stripped_strings = list(content_list[i].stripped_strings)
                 list_length = len(tag_stripped_strings)
                 content_len = list_length - 2
-                update_index = list_length - 1
                 # Content printer:
                 for string in range(0, content_len):
                     print(f"Content: {tag_stripped_strings[string]}")
+
+# extracts the time at which the trail conditions were updated
+def extract_update_times(trail_paragraph):
+    content_list = list(trail_paragraph.children)
+    if len(content_list) != 0:
+        print('extracting update times...')
+        # We start at the 19th position for the list of the trail paragraph as there is a blurb on the webpage that does not contain relevant content.
+        # In the future, it would be better to dynamically find this in the event that the website maintainers end up manually changing the contents, therefore ruining how this works
+        for i in range(19, len(content_list)):
+            if content_list[i].name == "p":
+                tag_stripped_strings = list(content_list[i].stripped_strings)
+                list_length = len(tag_stripped_strings)
+                update_index = list_length - 1
                 # Update Time printer:
                 print(f"Updated at: {tag_stripped_strings[update_index]}")
+
 
 soup = BeautifulSoup(page.content, 'html.parser')
 subsection = soup.find(id='block-threerivers-content')
@@ -47,10 +60,12 @@ subsection = soup.find(id='block-threerivers-content')
 traildiv =  soup.find("div", {"class": "userContent"})
 
 # gets us out of the trail div and into the main content
-drilldown = soup.find('p')
+trail_paragraph = soup.find('p')
 
-trail_status_extractor(drilldown)
+extract_trail_conditions(trail_paragraph)
 
-populate_park_list(drilldown)
+extract_update_times(trail_paragraph)
+
+populate_park_list(trail_paragraph)
 for park in parks:
     print(park)
